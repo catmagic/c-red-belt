a770dafb08050f5a25a2edc8866d40dfb96781d3
Сat Cheshire <catmagic000@gmail.com>
2023-07-31 17:53:52 +0300

    111

---
+++ 6weektask/3.1.cpp
@@ -0,0 +1,26 @@
+#include "test_runner.h"
+#include <vector>
+#include <future>
+using namespace std;
+
+int64_t CalculateMatrixSum(const vector<vector<int>>& matrix) {
+  // Реализуйте эту функцию
+  vector<future<int64_t>> futures;
+
+
+}
+
+void TestCalculateMatrixSum() {
+  const vector<vector<int>> matrix = {
+    {1, 2, 3, 4},
+    {5, 6, 7, 8},
+    {9, 10, 11, 12},
+    {13, 14, 15, 16}
+  };
+  ASSERT_EQUAL(CalculateMatrixSum(matrix), 136);
+}
+
+int main() {
+  TestRunner tr;
+  RUN_TEST(tr, TestCalculateMatrixSum);
+}
